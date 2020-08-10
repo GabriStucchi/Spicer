97b07c4dfb9c5c2cfd88d3162ba5c4a93233affc
Gabriele Stucchi <gabriele.stucchi@mail.polimi.it>
2020-08-10 11:08:59 +0200

    Fixed LFO

--- Spicer 2020/js/Synth/Lfo.js
+++ Spicer 2020/js/Synth/Lfo.js
@@ -1,17 +1,23 @@
 class Lfo {
     #lfo;
     #on = false;
+    #amp = 0;
 
     constructor(variation) {
         this.#lfo = new Tone.LFO("0.1", -variation, variation);
         this.#lfo.amplitude.value = 0;
     }
 
+    //Cambia i parametri dell'LFO. Se LFO è attivo amplitude viene modificata instantaneamente, se LFO non è attivo viene salvato in #amp il valore (che verrà applicato in toggle())
     changeParamValue(param, value) {
         if (param == 0)
             this.#lfo.frequency.value = value;
-        else if (param == 1)
-            this.#lfo.amplitude.value = value;
+        else if (param == 1){
+            if(this.#on)
+                this.#lfo.amplitude.value = value;
+            else
+                this.#amp = value;
+        }
     }
 
     changeWaveform(type) {
@@ -23,10 +29,15 @@ class Lfo {
     }
 
     toggle() {
-        if(this.#on)
+        if(this.#on){
             this.#lfo.stop();
-        else
+            this.#amp = this.#lfo.amplitude.value;
+            this.#lfo.amplitude.value = 0;
+        }
+        else{
+            this.#lfo.amplitude.value = this.#amp;
             this.#lfo.start();
+        }
         this.#on = !this.#on;
     }
 }
