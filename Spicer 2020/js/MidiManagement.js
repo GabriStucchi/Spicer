7270abf5cce3c15767863702a688c6a682fc21a7
LuigiAttorresi <luigi.attorresi@mail.polimi.it>
2020-08-26 12:34:06 +0200

    Add Chord recognition
    Add Chord recognition and chord class in spicer. Modified noteon/noteoff

--- Spicer 2020/js/MidiManagement.js
+++ Spicer 2020/js/MidiManagement.js
@@ -42,23 +42,23 @@ function  noteOn(pitch, timeStamp, velocity){
 
     if(playSynth){
         changeSynthNote(pitch);
+        activeNotes.push(pitch);
         synthNoteOn();
     }
     else{
         //suona piano
         instrumentNoteOn(pitch, velocity);
     }
-
-    activeNotes.push(pitch);
 }
 
-function noteOff(pitch, timeStamp){  
+function noteOff(pitch, timeStamp){
 
     let index = activeNotes.indexOf(pitch);
-   
-    activeNotes.splice(index, 1);
+
+
 
     if(playSynth){
+        activeNotes.splice(index, 1);
         if(activeNotes.length == 0)
             synthNoteOff();
         else if(index == activeNotes.length)
@@ -83,7 +83,7 @@ function fnPlayNote(pitch, octave) {
 
 }
 
-function instrumentNoteOn(pitch, velocity){ 
+function instrumentNoteOn(pitch, velocity){
     instrumentNoteOff(pitch);
     var envelope = player.queueWaveTable(audioContext, audioContext.destination, tone, 0, pitch, default_duration, velocity / 100);
     var note = {
@@ -91,6 +91,17 @@ function instrumentNoteOn(pitch, velocity){
         envelope: envelope
     };
     activeNotes.push(note);
+
+    //Controllo se viene suonato un accordo e in tal caso lo creo e riconosco
+    if (activeNotes.length>2 ) {
+      chord = new accordo
+
+      //Inserisco le active Notes al campo notes di chord
+      chord.addNote(activeNotes.pitch)
+      chordRecognition(chord)
+      console.log(chord);
+      //show(chord)
+    }
 }
 
 function instrumentNoteOff(pitch){
@@ -103,4 +114,18 @@ function instrumentNoteOff(pitch){
             return;
         }
     }
+}
+
+//Mostra l'accordo in console
+function show(chord){
+  if (chord.getNotes().length>2) {
+    root = chord.getRoot()
+
+    while (root>=12) {
+      root = root - 12
+    }
+    root = keys[root]
+    console.log('Accordo di ' + root + chord.getType() + ', ' + chord.getInversion()
+      + ' rivolto. Grado della scala: ' + chord.getGrade());
+  }
 }
